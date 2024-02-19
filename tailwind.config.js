a30bc285ae7945e8b88cfba84db2c0bbc2599cd4
devdvlive <thesocot@mail.ru>
2024-02-19 17:19:20 +0300

    ADD: articles section

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,7 @@ module.exports = {
         "linear-red-yellow": "linear-gradient(#F87171,#FDBA74)",
         "linear-yellow-blue": "linear-gradient(#FDBA74,#BFDBFE)",
         "linear-blue-pink": "linear-gradient(#BFDBFE,#F83A95)",
+        "linear-pink-red": "linear-gradient(#F83A95,#F81717)",
 
 
       },
