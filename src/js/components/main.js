60ae5ed0d3771684da4a1bd7b17b6ba00745dafa
devdvlive <thesocot@mail.ru>
2024-02-19 19:28:22 +0300

    ADD: video section w/ slider

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -5,26 +5,53 @@ document.addEventListener("DOMContentLoaded",() => {
 
   accordionButtons.forEach((button,i) => {
     button.addEventListener("click", () => {
-      if(accordionContents[i].classList.contains("grid-rows-[1fr]")) {
-        accordionContents[i].classList.remove("grid-rows-[1fr]");
-        accordionContents[i].classList.remove("opacity-100");
-        accordionContents[i].classList.add("grid-rows-[0fr]");
-        accordionContents[i].classList.add("opacity-0");
-        accordionContents[i].classList.add("grid-cols-2");
+      console.log(accordionContents[i].classList.contains('video-section'))
+      console.log(accordionContents[i].classList)
 
-        accordionContents[i].classList.remove("grid-cols-1");
-        accordionContents[i].classList.remove("lg:grid-cols-2");
+      if(accordionContents[i].classList.contains('video-section')){
+        if(accordionContents[i].classList.contains("grid-rows-[1fr]")) {
+          accordionContents[i].classList.remove("grid-rows-[1fr]");
+          accordionContents[i].classList.remove("opacity-100");
+          accordionContents[i].classList.add("grid-rows-[0fr]");
+          accordionContents[i].classList.add("opacity-0");
+          accordionContents[i].classList.add("grid-cols-2");
+          accordionContents[i].classList.remove("grid-cols-1");
 
-       }
-       else{
-        accordionContents[i].classList.remove("grid-rows-[0fr]");
-        accordionContents[i].classList.remove("opacity-0");
-        accordionContents[i].classList.add("grid-rows-[1fr]");
-        accordionContents[i].classList.add("opacity-100");
-        accordionContents[i].classList.add("grid-cols-1");
-        accordionContents[i].classList.add("lg:grid-cols-2");
-        accordionContents[i].classList.remove("grid-cols-2");
-       }
+
+         }
+         else{
+          accordionContents[i].classList.remove("grid-rows-[0fr]");
+          accordionContents[i].classList.remove("opacity-0");
+          accordionContents[i].classList.add("grid-rows-[1fr]");
+          accordionContents[i].classList.add("opacity-100");
+          accordionContents[i].classList.add("grid-cols-1");
+
+          accordionContents[i].classList.remove("grid-cols-2");
+
+         }
+      }else{
+        if(accordionContents[i].classList.contains("grid-rows-[1fr]")) {
+          accordionContents[i].classList.remove("grid-rows-[1fr]");
+          accordionContents[i].classList.remove("opacity-100");
+          accordionContents[i].classList.add("grid-rows-[0fr]");
+          accordionContents[i].classList.add("opacity-0");
+          accordionContents[i].classList.add("grid-cols-2");
+
+          accordionContents[i].classList.remove("grid-cols-1");
+          accordionContents[i].classList.remove("lg:grid-cols-2");
+
+         }
+         else{
+          accordionContents[i].classList.remove("grid-rows-[0fr]");
+          accordionContents[i].classList.remove("opacity-0");
+          accordionContents[i].classList.add("grid-rows-[1fr]");
+          accordionContents[i].classList.add("opacity-100");
+          accordionContents[i].classList.add("grid-cols-1");
+          accordionContents[i].classList.add("lg:grid-cols-2");
+          accordionContents[i].classList.remove("grid-cols-2");
+         }
+      }
+
     });
   })
 
