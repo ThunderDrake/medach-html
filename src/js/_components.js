962a89944ad9117c2cf671323bf75b9d617f8657
devdvlive <thesocot@mail.ru>
2024-02-26 15:49:41 +0300

    fix(video): оптимизация видео с youtube

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import { header } from "./components/header";
 import {main} from './components/main';
 import {searchModal} from './components/searchModal';
 import {archive} from './components/archive';
+import {video} from './components/video';
