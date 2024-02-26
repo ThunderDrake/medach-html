a5ddf8b59c958393989c04794c413fffeb0235c4
devdvlive <thesocot@mail.ru>
2024-02-26 16:24:58 +0300

    fix(js/components/main.js): фикс ошибки Cannot read properties of undefined (reading classList)

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -5,24 +5,29 @@ document.addEventListener("DOMContentLoaded",() => {
 
   accordionButtons.forEach((button,i) => {
     button.addEventListener("click", () => {
-        console.log('test ver')
-      if(button.childNodes[0].classList.contains('opacity-0')){
-        button.childNodes[0].classList.add('opacity-100');
-        button.childNodes[0].classList.remove('opacity-0');
-        button.childNodes[3].classList.add('opacity-0');
-        button.childNodes[3].classList.remove('opacity-100');
+        const seeButton = button.childNodes[1].classList ? button.childNodes[1] : button.childNodes[0];
+        const unseeButton = button.childNodes[3].classList ? button.childNodes[3] : button.childNodes[2];
+
+        console.log(seeButton)
+        console.log(unseeButton)
+
+      if(seeButton.classList.contains('opacity-0')){
+        seeButton.classList.add('opacity-100');
+        seeButton.classList.remove('opacity-0');
+        unseeButton.classList.add('opacity-0');
+        unseeButton.classList.remove('opacity-100');
         setTimeout(()=>{
-          button.childNodes[0].classList.remove('hidden');
-          button.childNodes[3].classList.add('hidden');
+          seeButton.classList.remove('hidden');
+          unseeButton.classList.add('hidden');
         },500)
       }else{
-        button.childNodes[0].classList.add('opacity-0');
-        button.childNodes[0].classList.remove('opacity-100');
-        button.childNodes[3].classList.remove('opacity-0');
-        button.childNodes[3].classList.add('opacity-100');
+        seeButton.classList.add('opacity-0');
+        seeButton.classList.remove('opacity-100');
+        unseeButton.classList.remove('opacity-0');
+        unseeButton.classList.add('opacity-100');
         setTimeout(()=>{
-          button.childNodes[0].classList.add('hidden');
-          button.childNodes[3].classList.remove('hidden');
+          seeButton.classList.add('hidden');
+          unseeButton.classList.remove('hidden');
         },500)
       }
 
