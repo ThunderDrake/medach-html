09ad8d2b00c5ce72acbcef6da7081ade54265473
devdvlive <thesocot@mail.ru>
2024-02-29 23:35:29 +0300

    REF: рефакторинг hero секции
    hero наконец переписана на grid
    
    todo: адаптация под разные экраны, hover эффекты для маленьких карточек

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,11 +26,12 @@ module.exports = {
         secondary: "#9CA3AF",
       },
       backgroundImage: {
-        "radial-gradient-red": "radial-gradient(circle 230px at  top left, rgba(248, 113, 113, .6), rgba(248, 113, 113, 0)), radial-gradient(circle 230px at  bottom right, rgba(248, 113, 113, .6), rgba(248, 113, 113, 0))",
-        "radial-gradient-green": "radial-gradient(circle 180px at  top left, rgba(179, 223, 152, .6), rgba(179, 223, 152, 0)),radial-gradient(circle 180px at  bottom right, rgba(179, 223, 152, .6), rgba(179, 223, 152, 0))",
-        "radial-gradient-orange": "radial-gradient(circle 180px at  top left, rgba(253, 186, 116, .6),rgba(179, 223, 152, 0)),radial-gradient(circle 180px at  bottom right, rgba(253, 186, 116, .6), rgba(179, 223, 152, 0))",
-        "radial-gradient-blue": "radial-gradient(circle 180px at  top left, rgba(191, 219, 254, .6),rgba(179, 223, 152, 0)),radial-gradient(circle 180px at  bottom right, rgba(191, 219, 254, .6), rgba(179, 223, 152, 0))",
-        "radial-gradient-pink": "radial-gradient(circle 180px at  top left, rgba(249, 168, 212, .6),rgba(179, 223, 152, 0)),radial-gradient(circle 180px at  bottom right, rgba(249, 168, 212, .6), rgba(179, 223, 152, 0))",
+        "radial-gradient-red": "radial-gradient(circle 330px at  top left, rgba(248, 113, 113, .6), rgba(248, 113, 113, 0)), radial-gradient(circle 280px at  bottom right, rgba(248, 113, 113, .6), rgba(248, 113, 113, 0))",
+        "radial-gradient-green": "radial-gradient(circle 280px at  top left, rgba(179, 223, 152, .6), rgba(179, 223, 152, 0)),radial-gradient(circle 230px at  bottom right, rgba(179, 223, 152, .6), rgba(179, 223, 152, 0))",
+        "radial-gradient-orange": "radial-gradient(circle 280px at  top left, rgba(253, 186, 116, .6),rgba(179, 223, 152, 0)),radial-gradient(circle 230px at  bottom right, rgba(253, 186, 116, .6), rgba(179, 223, 152, 0))",
+        "radial-gradient-blue": "radial-gradient(circle 280px at  top left, rgba(191, 219, 254, .6),rgba(179, 223, 152, 0)),radial-gradient(circle 230px at  bottom right, rgba(191, 219, 254, .6), rgba(179, 223, 152, 0))",
+        "radial-gradient-pink": "radial-gradient(circle 330px at  top left, rgba(249, 168, 212, .6),rgba(179, 223, 152, 0)),radial-gradient(circle 280px at  bottom right, rgba(249, 168, 212, .6), rgba(179, 223, 152, 0))",
+
         "linear-red-yellow": "linear-gradient(#F87171,#FDBA74)",
         "linear-yellow-blue": "linear-gradient(#FDBA74,#BFDBFE)",
         "linear-blue-pink": "linear-gradient(#BFDBFE,#F83A95)",
