85f2ab526318ead8690c4caa8d562be2d1cbf35a
devdvlive <thesocot@mail.ru>
2024-03-04 19:48:01 +0300

    ADD: закончена страница редактора

--- src/js/components/searchModal.js
+++ src/js/components/searchModal.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded",(e)=>{
-  const moreBtn = document.getElementById('more');
-  const tags = document.getElementById('tags');
-  moreBtn.addEventListener('click', () => {
-    moreBtn.childNodes[1].classList.toggle('rotate-180');
-    tags.classList.toggle('overflow-y-auto')
-    tags.classList.toggle('overflow-hidden')
+  const moreBtn = document.querySelectorAll('.more');
+  const tags = document.querySelectorAll('.tags');
+  moreBtn.forEach((btn,i)=>{
+    btn.addEventListener('click', () => {
+      btn.childNodes[1].classList.toggle('rotate-180');
+      tags[i].classList.toggle('overflow-y-auto')
+      tags[i].classList.toggle('overflow-hidden')
 
-  });
+    });
+  })
+  // moreBtn.addEventListener('click', () => {
+  //   moreBtn.childNodes[1].classList.toggle('rotate-180');
+  //   tags.classList.toggle('overflow-y-auto')
+  //   tags.classList.toggle('overflow-hidden')
+
+  // });
 })
